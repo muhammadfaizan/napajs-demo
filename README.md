bb785aad1d6417a093b7ab956e2291aaa03cdf56
Muhammad Faizan <muhammad.faizan@alibaba-inc.com>
2018-12-04 16:46:36 +0500

    long process update

--- README.md
+++ README.md
@@ -16,10 +16,21 @@ Note: I am also leaving code comments, and you can find references there.
 
 I think the output is funnyy, as it logged *hello world* 4 times without linefeed and then pushes all 4 linefeeds *\n* are pushed together, and after that it logs *hello napa* in the end. Well I tried running it again and got a different output, this time it logged 3 *hello world* on different lines and 4th one followed 3rd one without a linefeed, and then hello napa was printed after 1 blank line space.
 
-[![N|Solid](https://github.com/muhammadfaizan/napajs-demo/blob/master/example-1/example-1-output-2.png?raw=true)](https://github.com/muhammadfaizan/napajs-demo/blob/master/example-1/example-1-output-2.png)
+![N|Solid](https://github.com/muhammadfaizan/napajs-demo/blob/master/example-1/example-1-output-2.png?raw=true)
 
 
 May be we will find answers when we fall deeper, I think they are returning data to main thread in a stream, and a linefeed is getting disrupted by another thread input.
 So it means its concurrent and we can expect such sequences.
 
 **Right now I am curious about how zone.execute works?**
+
+
+## Example : long processing thread
+
+Here I am trying to modify an array of array, by slicing it up and passing it to thread, what I noticed is, that some data is not changing, while other is.
+
+    Output
+
+![N|Solid](https://github.com/muhammadfaizan/napajs-demo/blob/master/long-processing-threads/long-process-threads-output.png?raw=true)
+
+as you may notice it worked well and the process were working simultaneously and improved our plan. however I could not find why the output was a little off.. will check it out later.
--- long-processing-threads/index.js
+++ long-processing-threads/index.js
@@ -4,21 +4,29 @@ const THREADS_COUNT = 4;
 const zone = napa.zone.create('zone1', { workers: THREADS_COUNT });
 
 // The below variable is array has 40 elements for example && each element includes too much data
-let DATA = Array.from({length: 40}, (v, i) => i);
+let DATA = Array.from({length: 40}, v => 0);
+// thats not too much of data but instead 100 elements per data element, so thats a fair amount of number for this sample
+DATA = DATA.map((a) => Array.from({length: 100}, v => 0))
 
-DATA = DATA.map((a) => Array.from({length: 100}, (v, i) => i))
 
+// The below function takes long time && I have to call it for every element in DATA array variable
+// function ITakeLongTime(data) {}
 function ITakeLongTime(data, threadNo) {
     let threadName = `thread # ${threadNo}`;
     console.log(`${threadName} started`)
-    // console.time(threadNo)
-    while( i < 100000) { data[i%40][i%100] = i++ };
+    let i = 0;
+    // iterating to cause delay and modify data. just to check if data is being changed
+    while( i < 1000) { 
+        // modifying data here, in thread, but this wont effect global "dataSet" 
+        data[i % data.length][i%100] = i;
+        i++;
+        // uncomment below line to see what data is being changed, remember each data is slice of the original dataset.
+        // console.log(`${threadName}: data[${i % data.length}][${i%100}] = ${i}`)
+    }
     console.log(`${threadName} ended`)
     // console.endTime(threadNo);
-    return threadName;
+    return data;
 }
-// The below function takes long time && I have to call it for every element in DATA array variable
-// function ITakeLongTime(data) {}
 function run(dataSet = [], batches = 10) {
     var start = Date.now();
 
@@ -30,9 +38,18 @@ function run(dataSet = [], batches = 10) {
     }
     
     return Promise.all(promises).then(values => {
-        values.forEach(console.log);
-    })
-    .catch(console.log);;
-}
+        // returned value from function is in "value" attribute.
+        // the result is not what I expected...
+        // however you can manipulate or view your result here.
+        // values.forEach(result => console.log(result.value));
 
+        // console.log(values[0].value[0][21]);
+    })
+    .catch(err => {
+        //do something with error
+        console.log(err);
+    });;
+}
+// running code to 
 run(DATA, 10);
+
